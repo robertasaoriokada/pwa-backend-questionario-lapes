decff585fd07d7ce4f059c707452a02cdc254c41
Roberta <robertaokada144@gmail.com>
2024-04-22 19:53:10 -0300

    aaaaaa

--- src/main/java/com/questionario/jwt/SecurityConfig.java
+++ src/main/java/com/questionario/jwt/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-    http.csrf(csrf -> csrf.disable())
+    http.csrf.disable()
     .authorizeRequests(
       auth -> auth.requestMatchers("/authenticate").permitAll()
       .anyRequest().authenticated())

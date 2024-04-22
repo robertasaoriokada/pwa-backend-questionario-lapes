98a23e3d51956a7002ad0c16819d386a270c1cbb
Roberta <robertaokada144@gmail.com>
2024-04-22 19:49:37 -0300

    mais um pouco do jwt

--- src/main/java/com/questionario/jwt/SecurityConfig.java
+++ src/main/java/com/questionario/jwt/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.questionario.jwt;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.validation.annotation.Validated;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-  
+  @Value("${jwt.public.key}")
+  private RSAPublicKey pub;
+  @Value("${jwt.private.key}")
+  private RSAPrivateKey priv; 
+
   @Bean
-  SecurityFilterChain filterChain(HttpSecurity http){
-    http.csrf().disable()
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    http.csrf(csrf -> csrf.disable())
     .authorizeRequests(
       auth -> auth.requestMatchers("/authenticate").permitAll()
       .anyRequest().authenticated())
@@ -23,4 +40,17 @@ public class SecurityConfig {
     return http.build();
   }
 
+  @Bean
+  JwtDecoder jwtDecoder(){
+    return NimbusJwtDecoder.withPublicKey(pub).build();
+  }
+
+  @Bean
+  JwtEncoder jwtEncoder(){
+    //json web key
+    RSAKey jwk = new RSAKey.Builder(pub).privateKey(priv).build();
+    //json web keys
+    JWKSet jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
+  }
 }
